0618d7083fc2321ffa8915c2942560e85fa36706
Manita Intharachot <64160144@go.buu.ac.th>
2024-08-02 22:25:25 +0700

    fix createOrder in orderService

--- apps/order/src/order.service.ts
+++ apps/order/src/order.service.ts
@@ -20,14 +20,16 @@ export class OrderService {
     const menuItems = menuResponse.data;
     console.log('menuItems', menuItems);
     let total = 0;
+    const orderItem2 = [];
     for (const item of orderItems) {
       const menuItem = menuItems.find((mi) => mi.id === item.menuItemId);
+      orderItem2.push({ ...menuItem, quantity: item.quantity });
       if (!menuItem) {
         throw new NotFoundException(`Menu item ${item.menuItemId} not found`);
       }
       total += menuItem.price * item.quantity;
     }
-    const order = { id: this.orders.length + 1, items: orderItems, total };
+    const order = { id: this.orders.length + 1, items: orderItem2, total };
     this.orders.push(order);
     return order;
   }
